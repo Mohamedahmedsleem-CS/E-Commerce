00057107caf98d68a521006c2936c55151a84d00
Mohamedahmedsleem-CS <mohamedahmedsleem29511@gmail.com>
2024-12-23 00:13:50 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './index.css'
+
+function App() {
+  return (
+    <div >
+      <h1 className="allah" >
+      بسم الله الرحمن الرحيم 
+      </h1>
+      <p className='text-center'>ali</p>
+    </div>
+  );
+}
+
+export default App;
